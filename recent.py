f314de07eb34f42210d0a6c21be5aa8f79f5f36a
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2014-12-13 02:13:19 +0200

    Collect recent articles

---
+++ recent.py
@@ -0,0 +1,13 @@
+from common import *
+
+import json
+
+session = mendeley()
+
+recent = session.documents.list( page_size = 20, sort = 'created', order = 'desc' )
+
+recent = map( lambda publication: to_dict( publication ), recent.items )
+
+out = open('/var/www/main/data/latest.json', 'w')
+out.write( json.dumps( recent ) )
+out.close()
