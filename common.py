59f9a26b1f280bea824d122202f17a7a6f0ae9ec
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2014-12-13 02:12:42 +0200

    Create functions

--- common.py
+++ common.py
@@ -4,38 +4,49 @@ import yaml
 from mendeley import Mendeley
 from mendeley.session import MendeleySession
 
+def mendeley():
 
-## load configs from file
-conf = open('config.yml', 'r+w')
-config = yaml.load( conf )
+    ## load configs from file
+    conf = open('config.yml', 'r+w')
+    config = yaml.load( conf )
 
-mendeley = Mendeley(config['clientId'], config['clientSecret'], config['path'] )
+    mendeley = Mendeley(config['clientId'], config['clientSecret'], config['path'] )
 
-## interactive OAuth flow
-if 'token' not in config:
-  auth = mendeley.start_authorization_code_flow()
-  state = auth.state
-  auth = mendeley.start_authorization_code_flow( state = state )
-  print auth.get_login_url()
+    ## interactive OAuth flow
+    if 'token' not in config:
+      auth = mendeley.start_authorization_code_flow()
+      state = auth.state
+      auth = mendeley.start_authorization_code_flow( state = state )
+      print auth.get_login_url()
 
-  ## auth = mendeley.start_implicit_grant_flow()
-  # After logging in, the user will be redirected to a URL, auth_response.
-  session = auth.authenticate( raw_input() )
+      ## auth = mendeley.start_implicit_grant_flow()
+      # After logging in, the user will be redirected to a URL, auth_response.
+      session = auth.authenticate( raw_input() )
 
-  print session.token
-  config['token'] = session.token
+      print session.token
+      config['token'] = session.token
 
-  ## clean file
-  conf.write('')
-  yaml.dump( config, conf, default_flow_style=False )
-  print 'New infos stored'
+      ## clean file
+      conf.write('')
+      yaml.dump( config, conf, default_flow_style=False )
+      print 'New infos stored'
 
-## update access tokens
+    ## update access tokens
 
-## use new access token
-session = MendeleySession( mendeley, config['token'] )
+    ## use new access token
+    session = MendeleySession( mendeley, config['token'] )
 
-recent = session.documents.list( page_size = 20, sort = 'created', order = 'desc' )
+    return session
 
-for publication in recent.items:
-   print publication.title
+def to_dict( publication ):
+
+    data = {}
+
+    data['title'] = publication.title
+    data['year'] = publication.year
+    data['type'] = publication.type
+    data['venue'] = publication.source
+
+    data['authors'] = map( lambda person: { 'forename' : person.first_name , 'surname' : person.last_name } , publication.authors )
+
+    return data
