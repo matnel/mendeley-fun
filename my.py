ce6ebb60bd3cc01cf8f98456e1678d88c231469b
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2014-12-13 02:49:09 +0200

    Initial tagged version of my own works

---
+++ my.py
@@ -0,0 +1,48 @@
+from common import *
+
+import json
+
+session = mendeley()
+
+mine = []
+
+everything = session.documents.iter( page_size = 500, view = 'all' )
+
+for publication in everything:
+
+    if publication.authored and not publication.hidden:
+        mine.append( publication )
+
+mine = map( lambda publication: to_dict( publication ), mine )
+
+## transfer JUFO tags to something else
+for publication in mine:
+
+    jufo = 0
+
+    for i in ['1','2','3']:
+        if 'jufo' + i in publication['tags']:
+            jufo = i
+            publication['tags'].remove( 'jufo' + i )
+
+    publication['jufo'] = jufo
+
+## sort by tags
+
+tags = {}
+
+for publication in mine:
+
+        tag = 'other'
+
+        if publication['tags']:
+            tag = publication['tags'][0]
+
+        if tag not in tags:
+            tags[ tag ] = []
+
+        tags[ tag ].append( publication )
+
+out = open('/var/www/main/data/my.json', 'w')
+out.write( json.dumps( recent ) )
+out.close()
