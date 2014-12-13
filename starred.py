d7d4fc15c31baead20e23145795cee91859e633e
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2014-12-13 02:22:45 +0200

    Change sorting

--- starred.py
+++ starred.py
@@ -6,7 +6,7 @@ session = mendeley()
 
 starred = []
 
-everything = session.documents.iter( page_size = 500, view = 'client', sort = 'created', order = 'asc' )
+everything = session.documents.iter( page_size = 500, view = 'client', sort = 'created', order = 'desc' )
 
 for publication in everything:
 
