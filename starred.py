fcb0d62c175ff2ef24ded066ad9fcfb4cee02851
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2014-12-13 02:19:46 +0200

    Collect all starred documents

---
+++ starred.py
@@ -0,0 +1,20 @@
+from common import *
+
+import json
+
+session = mendeley()
+
+starred = []
+
+everything = session.documents.iter( page_size = 500, view = 'client' )
+
+for publication in everything:
+
+    if publication.starred:
+        starred.append( publication )
+
+starred = map( lambda publication: to_dict( publication ), starred )
+
+out = open('/var/www/main/data/star.json', 'w')
+out.write( json.dumps( starred ) )
+out.close()
